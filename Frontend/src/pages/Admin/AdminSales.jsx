30e1db618eec2a009f8c6a98d0026e813b14c259
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-14 21:23:25 +0530

    Update API URL for fetching sales data

--- Frontend/src/pages/Admin/AdminSales.jsx
+++ Frontend/src/pages/Admin/AdminSales.jsx
@@ -24,7 +24,7 @@ const AdminSales = () => {
     try {
       const accessToken = localStorage.getItem("accessToken");
       const res = await axios.get(
-        `${import.meta.env.VITE_URL}/api/order/sales`,
+        `${import.meta.env.VITE_API_URL}/api/order/sales`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
