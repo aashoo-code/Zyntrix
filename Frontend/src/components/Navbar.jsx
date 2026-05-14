7f25f72b05e1b4ba603290ea03ccfb6eb3f9be71
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-14 20:18:07 +0530

    Adding backend link

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   const logoutHandler = async () => {
     try {
       const res = await axios.post(
-        `http://localhost:5000/api/users/logout`,
+        `${import.meta.env.VITE_URL}/api/users/logout`,
         {},
         {
           headers: {
