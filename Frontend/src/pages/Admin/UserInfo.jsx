0571929638b5f0f766ee69ae41a598d1d4dbc192
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-14 20:33:25 +0530

    Update API URLs to use environment variable

--- Frontend/src/pages/Admin/UserInfo.jsx
+++ Frontend/src/pages/Admin/UserInfo.jsx
@@ -74,7 +74,7 @@ const UserInfo = () => {
       }
 
       const res = await axios.put(
-        `http://localhost:5000/api/users/update/${userId}`,
+        `${import.meta.env.VITE_URL}/api/users/update/${userId}`,
         formData,
         {
           headers: {
@@ -98,7 +98,7 @@ const UserInfo = () => {
   const getuserDetails = async () => {
     try {
       const res = await axios.get(
-        `http://localhost:5000/api/users/get-user/${userId}`,
+        `${import.meta.env.VITE_URL}/api/users/get-user/${userId}`,
       );
       if (res.data.success) {
         setUpdateUser(res.data.user);
