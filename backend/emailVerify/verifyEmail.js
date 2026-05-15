1aebc06f2527cd6669a13add6154610b2d647935
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-15 13:12:36 +0530

    Refactor email verification logic and transporter setup

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -3,28 +3,40 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP ERROR:", error);
-  } else {
-    console.log("SMTP SERVER READY");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("SMTP ERROR:", error);
+//   } else {
+//     console.log("SMTP SERVER READY");
+//   }
+// });
 
 export const verifyEmail = async (email, token) => {
   try {
+    const {name, email, message} = req.body;
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {  user: process.env.MAIL_USER,
+               pass: process.env.MAIL_PASS,
+  },
+secure: true,
+    });
+
+    
     const mailConfiguration = {
       from: process.env.MAIL_USER,
       to: email,
       subject: "Email Verification",
+      text: message,
       html: `
         <p>Please click the link below to verify your email:</p>
         <a href="https://zyntrix-frontend.onrender.com/verify/${token}">
@@ -33,10 +45,18 @@ export const verifyEmail = async (email, token) => {
       `,
     };
 
-    const info = await transporter.sendMail(mailConfiguration);
-
-    console.log("Email sent:", info.response);
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if(err) {
+          console.error(err);
+          reject(err)
+        } else{
+          resolve(info);
+        }
+      });
+    });
+    res.status(200).json({ message: "Email Sent"});
   } catch (error) {
-    console.log("Error sending email:", error);
+    res.status(500).json({message: error.message});
   }
 };
