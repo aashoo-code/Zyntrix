f33e44912b66b47b1477e2f3ab7a9d2eee952e9d
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-15 16:41:32 +0530

    Improve email verification template design
    Updated the email verification HTML template to enhance styling and user experience.

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -42,8 +42,72 @@ export const verifyEmail = async (email, token) => {
       from: "Zyntrix <onboarding@resend.dev>",
       to: email,
       subject: "Verify Your Email",
-      html: `<p>Hi! There, You have recently visited our website and entered your email, </br > Please click the following link to verify your email:</p>
-//            <a href="https://zyntrix-frontend.onrender.com//verify/${token}">Verify Email</a>`,
+     html: `
+  <div style="
+    margin:0;
+    padding:40px 20px;
+    background: linear-gradient(to bottom right, #020617, #0f172a, #1e293b);
+    font-family: Arial, sans-serif;
+    color: white;
+    min-height: 100vh;
+  ">
+
+    <div style="
+      max-width: 500px;
+      margin: auto;
+      background: rgba(15, 23, 42, 0.9);
+      border: 1px solid rgba(34, 211, 238, 0.3);
+      border-radius: 20px;
+      padding: 40px 30px;
+      text-align: center;
+      box-shadow: 0 0 30px rgba(34, 211, 238, 0.15);
+    ">
+
+      <h1 style="
+        color: #22d3ee;
+        margin-bottom: 20px;
+        font-size: 28px;
+      ">
+        Verify Your Email
+      </h1>
+
+      <p style="
+        color: #cbd5e1;
+        font-size: 16px;
+        line-height: 1.7;
+        margin-bottom: 30px;
+      ">
+        Welcome to Zyntrix ✨ <br/><br/>
+Hi! There, You have recently visited our website and entered your email, </br > Please verify your email address to Activate your Account.
+      </p>
+
+      <a
+        href="${process.env.CLIENT_URL}/verify/${token}"
+        style="
+          display: inline-block;
+          padding: 14px 28px;
+          background: linear-gradient(to right, #06b6d4, #3b82f6);
+          color: white;
+          text-decoration: none;
+          border-radius: 12px;
+          font-weight: bold;
+          font-size: 16px;
+        "
+      >
+        Verify Email
+      </a>
+
+      <p style="
+        margin-top: 30px;
+        color: #64748b;
+        font-size: 13px;
+      ">
+        If you didn’t create this account, you can safely ignore this email.
+      </p>
+
+    </div>
+  </div>
+`,
     });
 
     console.log("EMAIL SENT =>", response);
