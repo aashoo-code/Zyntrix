6db90f67bc3bab0916cf67f962b58e315f31e270
Ashu <140695297+aashoo-code@users.noreply.github.com>
2026-05-15 12:27:43 +0530

    Refactor verifyEmail to use async/await

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const verifyEmail = (email, token) => {
+export const verifyEmail = async (email, token) => {
    export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -11,19 +11,23 @@ export const verifyEmail = (email, token) => {
   },
 });
 
-  const mailConfiguration = {
-    from: process.env.MAIL_USER,
-    to: email,
-    subject: "Email Verification",
-    html: `<p>Hi! There, You have recently visited our website and entered your email, </br > Please click the following link to verify your email:</p>
-           <a href="https://zyntrix-frontend.onrender.com/verify/${token}">Verify Email</a>`,
-  };
+   try {
+    const mailConfiguration = {
+      from: process.env.MAIL_USER,
+      to: email,
+      subject: "Email Verification",
+      html: `
+        <p>Please click the link below to verify your email:</p>
+        <a href="https://zyntrix-frontend.onrender.com/verify/${token}">
+          Verify Email
+        </a>
+      `,
+    };
 
-  transporter.sendMail(mailConfiguration, (error, info) => {
-    if (error) {
-      console.log("Error sending email:", error);
-    } else {
-      console.log("Email sent successfully:", info.response);
-    }
-  });
+    const info = await transporter.sendMail(mailConfiguration);
+
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.log("Error sending email:", error);
+  }
 };
