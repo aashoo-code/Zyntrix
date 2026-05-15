c42f24786e9064b7f0dab8a6deb8871f73d410e8
unknown <ashu0986ak@gmail.com>
2026-05-15 14:03:55 +0530

    Replace nodemailer with resend

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -1,4 +1,35 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const verifyEmail = (email, token) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.MAIL_USER,
+//       pass: process.env.MAIL_PASS,
+//     },
+//   });
+
+//   const mailConfiguration = {
+//     from: process.env.MAIL_USER,
+//     to: email,
+//     subject: "Email Verification",
+//     html: `<p>Hi! There, You have recently visited our website and entered your email, </br > Please click the following link to verify your email:</p>
+//            <a href="https://zyntrix-frontend.onrender.com//verify/${token}">Verify Email</a>`,
+//   };
+
+//   transporter.sendMail(mailConfiguration, (error, info) => {
+//     if (error) {
+//       console.log("Error sending email:", error);
+//     } else {
+//       console.log("Email sent successfully:", info.response);
+//     }
+//   });
+// };
+
+
+import { Resend } from "resend";
 import dotenv from "dotenv";
 
 dotenv.config();
